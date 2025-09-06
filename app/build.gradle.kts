c676a90174e2762837442da06f3e2ed102563a39
Saber <ahmad.saber@sitech.me>
2025-09-06 16:42:13 +0300

    Apply kotlinx-serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlinx.serialization)
     id("org.jetbrains.kotlin.kapt")
 }
 
@@ -35,6 +36,12 @@ android {
     buildFeatures {
         compose = true
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    composeOptions.kotlinCompilerExtensionVersion = "1.5.15"
 }
 
 kotlin {
