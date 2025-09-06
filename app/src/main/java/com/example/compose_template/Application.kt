4a95647f496b9ae0846d82b54e6bf0c2a8fe6c0c
Saber <ahmad.saber@sitech.me>
2025-09-06 16:43:14 +0300

    Add hilt application class

---
+++ app/src/main/java/com/example/compose_template/Application.kt
@@ -0,0 +1,7 @@
+package com.example.compose_template
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeTemplateApplication : Application()
