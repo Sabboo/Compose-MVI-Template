2d86f38d5ff292929dc19bfaf6d117fcb91c5106
Saber <ahmad.saber@sitech.me>
2025-09-06 19:59:27 +0300

    Added UI model with mapper

---
+++ app/src/main/java/com/example/compose_template/features/character_list/presentation/mapper/CharacterMapper.kt
@@ -0,0 +1,11 @@
+package com.example.compose_template.features.character_list.presentation.mapper
+
+import com.example.compose_template.features.character_list.presentation.model.CharacterUi
+import com.example.compose_template.features.character_list.domain.model.Character
+
+fun Character.toUi(): CharacterUi = CharacterUi(
+    id = id,
+    name = name,
+    species = species,
+    image = image,
+)
---
+++ app/src/main/java/com/example/compose_template/features/character_list/presentation/model/CharacterUi.kt
@@ -0,0 +1,8 @@
+package com.example.compose_template.features.character_list.presentation.model
+
+data class CharacterUi(
+    val id: Int,
+    val name: String,
+    val species: String,
+    val image: String
+)
